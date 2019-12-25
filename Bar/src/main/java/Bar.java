a0e2f922ed3ee25cfc2693e36d04c3b878ac189f
Rost Zhiharev <revers914@gmail.com>
2019-12-25 13:52:10 +0400

    wip

---
+++ Bar/src/main/java/Bar.java
@@ -0,0 +1,7 @@
+public class Bar {
+
+    public String bar() {
+        return "bar";
+    }
+
+}
---
+++ Foo/src/main/java/Foo.java
@@ -0,0 +1,8 @@
+public class Foo {
+
+    public static void main(String[] args) {
+        Bar bar = new Bar();
+        System.out.println(bar.bar());
+    }
+
+}
